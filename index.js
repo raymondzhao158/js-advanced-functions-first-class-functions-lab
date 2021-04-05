e64987ed05ccef5dc43bcb06dd4e57ce0828316d
raymond zhao <raymondzhao158@gmail.com>
2021-04-05 14:23:39 -0700

    commit tree 4b222820f6133b3f331884af00e6e287ce093124

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driverArray) {
+    const firstTwoDrivers =  driverArray.slice(0 , 2)
+    return firstTwoDrivers
+};
+
+function returnLastTwoDrivers(driverArray) {
+    const lastTwoDrivers = driverArray.slice(Math.max(driverArray.length - 2, 0))
+    return lastTwoDrivers
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function(fare) {
+        return (fare*fare)
+    }
+};
+
+function fareDoubler(fare) {
+    return (fare*2);
+};
+
+function fareTripler(fare) {
+    return (fare*3)
+};
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers)
+};
